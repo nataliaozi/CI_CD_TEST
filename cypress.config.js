84bbed6d8b1e7fbbb8f13b69839e4a3a16c66534
nataliaozi <natalia.ozlanska@gmail.com>
2025-06-24 15:34:53 +0200

    Fix cypress.config.js with correct specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
